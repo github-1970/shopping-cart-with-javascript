21e37b085cf928ec75446d0835026d865d86b496
github-1970 <github1970@gmail.com>
2022-01-13 12:23:57 +0330

    end modal and start convert procedural to OOP programing...

---
+++ src/js/Modal.js
@@ -0,0 +1,9 @@
+export default class Modal {
+  constructor() {
+    this.modalContainer = document.querySelector('.modal-container')
+    this.modal = document.querySelector('.modal')
+    this.modalBtn = document.querySelector('.modal-btn')
+    this.closeBtn = document.querySelector('.close-modal')
+    this.successBtn = document.querySelector('.btn-success')
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,52 @@
 import '@fortawesome/fontawesome-free/js/all'
 import '../scss/main.scss'
+import Modal from './Modal'
+
+const modals = new Modal()
+
+
+
+
+
+
+console.log(modals.closeBtn)
+
+const modalContainer = document.querySelector('.modal-container')
+const modal = document.querySelector('.modal')
+const modalBtn = document.querySelector('.modal-btn')
+const closeBtn = document.querySelector('.close-modal')
+const successBtn = document.querySelector('.btn-success')
+
+modalBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  modalContainer.style.zIndex = '1'
+  modalContainer.style.opacity = '1'
+
+  modal.style.opacity = '1'
+  modal.style.transform = 'translateY(0)'
+})
+
+closeBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  closeModal()
+})
+
+successBtn.addEventListener('click', () => {
+  closeModal()
+})
+
+window.addEventListener('click', (e) => {
+  if (e.target == modalContainer) {
+    closeModal()
+  }
+})
+
+function closeModal(){
+  modalContainer.style.zIndex = '-1'
+  modalContainer.style.opacity = '0'
+
+  modal.style.opacity = '0'
+  modal.style.transform = 'translateY(-100%)'
+}
